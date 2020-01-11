83a8691ded2b441f029781e0aa3cd2ab91dc6815
bhavesh80 <choudharybhavesh5@gmail.com>
2020-01-11 09:38:59 +0000

    articles update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,58 +1,59 @@
 class ArticlesController < ApplicationController
-before_action :set_article, only: [:edit, :update , :show ,:destroy]
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
-    def index
-        @articles = Article.paginate(page: params[:page], per_page: 5)
+  def index
+    @articles = Article.paginate(page: params[:page], per_page: 5)
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.user = User.first
+
+    if @article.save
+      flash[:success] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
     end
+  end
 
-    def new
-        @article = Article.new
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+    if @article.update(article_params)
+      flash[:success] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
+  end
 
-    def create
-        @article = Article.new(article_params)
-        @article.user = User.first
+  def destroy
 
-        if @article.save
-            flash[:success] = "Article was successfully created"
-            redirect_to article_path(@article)
-        else
-            render 'new'
-        end
-    end
+    @article.destroy
+    flash[:danger] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
 
-    def show
+  private
 
-    end
-
-    def edit
-
-    end
-
-    def update
-
-        if @article.update(article_params)
-            flash[:success] = "Article was successfully updated"
-            redirect_to article_path(@article)
-        else
-            render 'edit'
-        end
-    end
-    def destroy
-
-        @article.destroy
-        flash[:danger] = "Article was successfully deleted"
-        redirect_to articles_path
-    end
-
-private
-
-def set_article
+  def set_article
     @article = Article.find(params[:id])
-end
+  end
 
- def article_params
- params.require(:article).permit(:title, :description)
- end
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 
 end
